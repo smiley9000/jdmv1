a41317a727dffc4f1393c15e9368d0734ccbb570
Smiley <59495159+smiley9000@users.noreply.github.com>
2024-10-07 09:02:54 +0530

    initial

--- libinit/init_xiaomi_earth.cpp
+++ libinit/init_xiaomi_earth.cpp
@@ -10,55 +10,32 @@
 
 #include "vendor_init.h"
 
-static const variant_info_t aether_info = {
+static const variant_info_t a055f_info = {
     .hwc_value = "",
-    .sku_value = "aether",
+    .sku_value = "a05m",
 
-    .brand = "Redmi",
-    .device = "aether",
-    .marketname = "Redmi 12C",
-    .model = "22126RN91Y",
-    .build_fingerprint = "Redmi/aether_global/aether:14/UP1A.231005.007/V816.0.2.0.UCVMIXM:user/release-keys"
+    .brand = "Samsung",
+    .device = "a05m",
+    .marketname = "Samsung Galaxy A05",
+    .model = "SM-A055M",
+    .build_fingerprint = "samsung/a05mxx/a05m:12/SP1A.210812.016/A055FXXU4CXF1:user/release-keys"
 };
 
-static const variant_info_t earth_info = {
+static const variant_info_t a055m_info = {
     .hwc_value = "",
-    .sku_value = "earth",
+    .sku_value = "a05m",
 
-    .brand = "Redmi",
-    .device = "earth",
-    .marketname = "Redmi 12C",
-    .model = "22120RN86G",
-    .build_fingerprint = "Redmi/earth_global/earth:14/UP1A.231005.007/V816.0.2.0.UCVMIXM:user/release-keys"
+    .brand = "Samsung",
+    .device = "a05m",
+    .marketname = "Samsung Galaxy A05",
+    .model = "SM-A055F",
+    .build_fingerprint = "samsung/a05mxx/a05m:12/SP1A.210812.016/A055FXXU4CXF1:user/release-keys"
 };
 
-static const variant_info_t earth_la_info = {
-    .hwc_value = "",
-    .sku_value = "earth_la",
-
-    .brand = "Redmi",
-    .device = "earth",
-    .marketname = "Redmi 12C",
-    .model = "2212ARNC4L",
-    .build_fingerprint = "Redmi/earth_global/earth:14/UP1A.231005.007/V816.0.2.0.UCVMIXM:user/release-keys"
-};
-
-static const variant_info_t earth_p_info = {
-    .hwc_value = "",
-    .sku_value = "earth_p",
-
-    .brand = "Poco",
-    .device = "earth",
-    .marketname = "POCO C55",
-    .model = "22127PC95G",
-    .build_fingerprint = "POCO/earth_p_global/earth:14/UP1A.231005.007/V816.0.2.0.UCVMIXM:user/release-keys"
-};
 
 static const std::vector<variant_info_t> variants = {
-    aether_info,
-    earth_info,
-    earth_la_info,
-    earth_p_info,
+    a055f_info,
+    a055m_info,
 };
 
 void vendor_load_properties() {
